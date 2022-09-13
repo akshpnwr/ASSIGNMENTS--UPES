9a53fcb5dfef3b2b40625c02be3517701a43352f
Akash Panwar <akashpanwar9976@gmail.com>
2022-09-13 10:57:37 +0530

    LAB 3 experiment 1

---
+++ DSA/LAB 3/Exp-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int num, a;
+    char choice = 'Y';
+    int countPositive = 0, countNegative = 0, countZero = 0;
+    while (choice == 'Y' || choice == 'y')
+    {
+        printf("\nPlease enter a number:");
+        scanf("%d", &num);
+
+        if (num > 0)
+        {
+            countPositive++;
+        }
+        else if (num < 0)
+        {
+            countNegative++;
+        }
+        else
+        {
+            countZero++;
+        }
+
+        printf("Do you want to continue (y/n) ?");
+        scanf(" %c", &choice);
+    }
+
+    printf("\nPositive numbers: %d", countPositive);
+    printf("\nNegative numbers: %d", countNegative);
+    printf("\nZeroes: %d", countZero);
+
+    return 0;
+}
