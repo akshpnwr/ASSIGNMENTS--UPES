623c9b45ff81514b8af2224487ad51c544867f0e
Akash Panwar <akashpanwar9976@gmail.com>
2022-10-20 20:33:48 +0530

    Python Exp 4 ques 1 completed

--- Python/Experiment 4/libraries.py
+++ Python/Experiment 4/libraries.py
@@ -2,6 +2,10 @@
 # n1 = int(input('Enter a number : '))
 # n2 = int(input('Enter another number : '))
 
+import math
+from random import randint, random
+
+
 n1 = 4
 n2 = 9
 
@@ -17,3 +21,22 @@ print('Evaluating "1+2" through eval() : ' + str(eval('1+2')))
 
 prog = 'print("The sum of 5 and 10 is", (5+10))'
 exec(prog)
+
+print('Unicode of 65 107 97 115 104 "using chr()"= ', end='')
+print(chr(65), chr(107),chr(97),chr(115),chr(104))
+print('ASCII of A K A S H "using ord()"= ', end='')
+print(ord('A'), ord('k'),ord('a'),ord('s'),ord('h'))
+
+print('Round of 6.7 : ' + str(round(6.7)))
+
+print('Random Number : ' + str(random()))
+print('Random Integer from 1-5 : ' + str(randint(1,5)))
+
+print('Sin of π/2 : '+ str(math.sin(math.pi/2)))
+print('Cos of 0 : '+ str(math.cos(0)))
+print('Tan of 0 : '+ str(math.tan(0)))
+
+print('Ceil of 7.8 : ' + str(math.ceil(7.8)))
+print('Floor of 7.8 : ' + str(math.floor(7.8)))
+
+print('Pi in degrees : '+ str(math.degrees(math.pi)))
