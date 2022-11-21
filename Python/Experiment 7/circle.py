07a46400d3d97b170e7df7a6e162fbd9e5fa06f7
Akash Panwar <akashpanwar9976@gmail.com>
2022-11-21 21:21:58 +0530

    Python Lab 7 Exp 1 & 2

---
+++ Python/Experiment 7/circle.py
@@ -0,0 +1,17 @@
+import math
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius*self.radius
+    def circumference(self):
+        return 2*math.pi*self.radius
+
+
+radius = int(input('Please enter radius of the circle : '))
+c1 = Circle(radius)
+print('Area of the circle with radius {} is {}'.format(radius, c1.area()))
+print('Circumference of the circle with radius {} is {}'.format(radius, c1.circumference()))
+
+print(4 + 3 % 5)
---
+++ Python/Experiment 7/rectangle.py
@@ -0,0 +1,21 @@
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+    
+    def area(self):
+        return self.length*self.breadth;
+
+    def perimeter(self):
+        return 2*(self.length + self.breadth)
+
+
+length = int(input('Please enter length of the rectangle : '))
+breadth = int(input('Please enter breadth of the rectangle : '))
+
+r1 = Rectangle(length,breadth)
+
+
+print('The length and breadth of the rectangle is {} {}'.format(r1.length, r1.breadth))
+print('Area of the rectangle is {}'.format(r1.area()))
+# print(r1.perimeter())
