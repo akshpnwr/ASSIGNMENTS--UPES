4d9ae3b6c5dd5c86103c444018c4463ee9ce97d1
Akash Panwar <akashpanwar9976@gmail.com>
2022-11-30 21:22:09 +0530

    DSA Largest subarray equal to k

--- DSA/LAB 12/sum_equal_to_k.c
+++ DSA/LAB 12/sum_equal_to_k.c
@@ -1,25 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main() {
-
-    int size, target;
-    printf("Enter size of the array : ");
-    scanf("%d",&size);
-
-    int arr[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rand()%100+1;
-    }
-
-    printf("Enter the target sum : ");
-    scanf("%d",&target);
-
+int smallest(int arr[], int size,int target, int* small, int* large) {
+    
     int sum = 0;
     int l = 0, r = 0;
-    int min = 0;
+    int temp = 0;
 
     for (int i = 0; i < size; i++)
     {
@@ -31,18 +17,52 @@ int main() {
         }
         if (sum == target)
         {
-            min = r - l + 1;
+            temp = r - l + 1;
+            if (*small == 0 || *small > temp)
+            {
+                *small = temp;
+            }
+
+            if (*large == 0 || *large < temp)
+            {
+                *large = temp;
+            }
+
             sum = sum - arr[l];
             l++;
         }
         
         r++;
     }
-    
+
+}
+
+int main() {
+
+    int size, target;
+    printf("Enter size of the array : ");
+    scanf("%d",&size);
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand()%10+1;
+    }
+
+    printf("Enter the target sum : ");
+    scanf("%d",&target);
+
+    int smallest_subarray_size = 0, largest_subarray_size = 0;
+
+    smallest(arr, size, target, &smallest_subarray_size, &largest_subarray_size);
+
     printf("Randomly generated array : ");
     for (int i = 0; i < size; i++) printf("%d ",arr[i]);
 
-    printf("\nSize of smallest sub-array whose sum is equal to %d : %d",target,min);
+    printf("\nSize of smallest sub-array whose sum is equal to %d : %d",target,smallest_subarray_size);
+    printf("\nSize of largest sub-array whose sum is equal to %d : %d",target,largest_subarray_size);
+
 
 return 0;
 }
