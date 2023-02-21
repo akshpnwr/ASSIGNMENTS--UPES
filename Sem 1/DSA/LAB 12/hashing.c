75ffb29e30c9004b06b01a5ec1d1300bb0693be1
Akash Panwar <akashpanwar9976@gmail.com>
2023-02-21 11:15:46 +0530

    Removed soft computing

--- Sem 1/DSA/LAB 12/hashing.c
+++ Sem 1/DSA/LAB 12/hashing.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct hash {
+// Array, Structures, Linked List, Hashing, pointers, dynamic memory allocation
+
+struct hash
+{
     int value;
     struct hash *next;
 };
@@ -10,48 +13,51 @@ typedef struct hash hash;
 
 #define MAX_SIZE 10
 
-hash* arr[MAX_SIZE];
+hash *arr[MAX_SIZE];
 
-hash* createNode(int value) {
+hash *createNode(int value)
+{
 
-    hash* node = (hash*) malloc(sizeof(hash));
+    hash *node = (hash *)malloc(sizeof(hash));
     node->value = value;
     node->next = NULL;
 
     return node;
 }
 
-void init() {
-    for (int i = 0; i < MAX_SIZE; i++) arr[i] = createNode(-1); 
+void init()
+{
+    for (int i = 0; i < MAX_SIZE; i++)
+        arr[i] = createNode(-1);
 }
 
-void performChaining(int value) {
+void performChaining(int value)
+{
 
-        int mod = value%MAX_SIZE;
+    int mod = value % MAX_SIZE;
 
-        hash* temp = arr[mod];
-        hash* node = createNode(value);
-        
-        while (temp->next!=NULL)
-        {
-            temp = temp->next;
-        }
-        
-        temp->next = node;
+    hash *temp = arr[mod];
+    hash *node = createNode(value);
 
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = node;
 }
 
-void linearProbing(int value, int c) {
-    
-    int index = (value % MAX_SIZE+ c) % MAX_SIZE;
+void linearProbing(int value, int c)
+{
+
+    int index = (value % MAX_SIZE + c) % MAX_SIZE;
 
     while (arr[index]->value != -1)
     {
-        index = (value % MAX_SIZE+ c) % MAX_SIZE;
+        index = (value % MAX_SIZE + c) % MAX_SIZE;
         c++;
-
     }
-    
+
     arr[index]->value = value;
 
     // for (int i = 0; i < MAX_SIZE; i++)
@@ -61,22 +67,24 @@ void linearProbing(int value, int c) {
     //         arr[i]->value = value;
     //         break;
     //     }
-        
+
     // }
-    
 }
 
-void insert(int value) {
+void insert(int value)
+{
 
-    if (arr[value%MAX_SIZE]->value == -1) arr[value % MAX_SIZE] = createNode(value);
-    else {
-        // performChaining(value);
-        linearProbing(value, 0);
+    if (arr[value % MAX_SIZE]->value == -1)
+        arr[value % MAX_SIZE] = createNode(value);
+    else
+    {
+        performChaining(value);
+        // linearProbing(value, 0);
     }
-    
 }
 
-void display() {
+void display()
+{
     for (int i = 0; i < MAX_SIZE; i++)
     {
         if (arr[i]->value != -1)
@@ -84,44 +92,45 @@ void display() {
             if (arr[i]->next != NULL)
             {
                 printf("\n");
-                
-                while (arr[i]!=NULL)
+
+                while (arr[i] != NULL)
                 {
-                    printf("%d ",arr[i]->value);
+                    printf("%d ", arr[i]->value);
                     arr[i] = arr[i]->next;
                 }
             }
-            
-            else printf("\n%d",arr[i]->value);
-            
-        }
-        else printf("\nNULL");
 
+            else
+                printf("\n%d", arr[i]->value);
+        }
+        else
+            printf("\nNULL");
     }
 }
 
-int main() {
-
-init();
-
-int keys[] = {23,46,12,55,17,33,51,73};
-
-int keysSize = sizeof(keys)/sizeof(int);
-
-printf("\nKeys to be inserted : ");
-for (int i = 0; i < keysSize; i++)
+int main()
 {
-    printf("%d ", keys[i]);
-}
 
+    init();
 
-for (int i = 0; i < keysSize; i++)
-{
-    insert(keys[i]);
-}
+    int keys[] = {23, 46, 12, 55, 17, 33, 51, 73};
 
-printf("\n\nInsertion completed !!\n");
-display();
+    // keysSize = 8
+    int keysSize = sizeof(keys) / sizeof(int);
 
-return 0;
+    printf("\nKeys to be inserted : ");
+    for (int i = 0; i < keysSize; i++)
+    {
+        printf("%d ", keys[i]);
+    }
+
+    for (int i = 0; i < keysSize; i++)
+    {
+        insert(keys[i]);
+    }
+
+    printf("\n\nInsertion completed !!\n");
+    display();
+
+    return 0;
 }
